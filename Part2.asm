a1bd78aa5bd707d682fa336b444a3ed5b6bf0cc9
Richard Cavell <richardcavell@mail.com>
2025-07-10 10:25:25 +0100

    Testing changes

--- Part2.asm
+++ Part2.asm
@@ -263,8 +263,9 @@ _wait_for_vblank_and_check_for_skip_loop:
 	bra	_wait_for_vblank_and_check_for_skip_loop
 
 _wait_for_vblank:
-	tst	vblank_happened
-	beq	_wait_for_vblank_and_check_for_skip_loop
+	tst	$ff03
+	bpl	_wait_for_vblank_and_check_for_skip_loop
+	tst	$ff02
 
 	clra		; A VBlank happened
 	rts
